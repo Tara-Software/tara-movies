7ebe68d690ac9f25d510037d1d7bf7eaac8a7e0a
daiant <cmg2512@gmail.com>
2021-09-02 14:33:22 +0200

    ASLKDJH

--- pages/signup.js
+++ pages/signup.js
@@ -18,7 +18,7 @@ export default function Signup() {
         e.preventDefault()
         
         const hashed_password = hash_password(password);
-
+        console.log(process.env.API_URL + "/api/auth/signup")
         const response = await fetch(`${process.env.API_URL}/api/auth/signup`, {
             method: 'POST',
             body: JSON.stringify({name: username, email: email, password: hashed_password})
