232d62f4870d24e5f1eb65d7852941982dd5a8e1
daiant <cmg2512@gmail.com>
2021-09-02 14:59:34 +0200

    delete migrations

--- prisma/migrations/20210805161952_unique_title_movie/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title]` on the table `Movie` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Movie.title_unique" ON "Movie"("title");
