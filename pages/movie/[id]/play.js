3bf9ba8e7a31be9c6d7c1d9296ddcfc5fe0884da
daiant <cmg2512@gmail.com>
2021-09-03 09:49:59 +0200

    lo de antes pero con una s

--- pages/movie/[id]/play.js
+++ pages/movie/[id]/play.js
@@ -188,7 +188,7 @@ export default function Movie({ user, movie, data}) {
                                 onDuration={handleDuration} 
                                 className="video-player" 
                                 onProgress={handleProgress} 
-                                url={`http://95.22.168.100:7777/videos/${movie}`} 
+                                url={`https://95.22.168.100:7777/videos/${movie}`} 
                                 playing={playing} 
                                 muted={mute} 
                                 volume={Number(volume)}
